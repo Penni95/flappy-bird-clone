e4a5e7ef0184e2687045d7b46059b556cb4406ef
Benjamin Kuske <benjamin.kuske@web.de>
2022-06-30 22:00:50 +0200

    Displaying Picture

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 const config = {
   type: Phaser.AUTO, // sets WebGL (Web Graphics Library - JS API 2D/3D renderer) as default
   width: 800,
   height: 600,
   physics: {
-    // arcade physics plugin, manages physic simulations
-    default: 'arcade'
+    default: 'arcade' // arcade physics plugin, manages physic simulations
   },
   scene: {
-    preLoad, //instead of preLoader: preLoader - JS Shorthand
+    preload, //instead of preLoader: preLoader - JS Shorthand
     create
   }
 };
 
+
 // loads assets like images, music, animations ...
-function preLoad(){
+function preload(){
   // 'this' context - scene
   // contains functions and properties we can use
-  debugger
+  this.load.image('sky', 'assets/sky.png'); // 1. parameter is key
+  // console.log('test1');
 }
 
 function create(){
-  debugger
+  // parameters: 
+  // x = 400
+  // y = 300
+  // key of image
+  this.add.image(config.width / 2, config.height / 2, 'sky');
+  // console.log('test2');
 }
 
 new Phaser.Game(config);
